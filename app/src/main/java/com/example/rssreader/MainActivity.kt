e3b361a189725bd7860139c954f4668dbde11f28
liam <wonhyuk1994@gmail.com>
2024-11-15 19:00:31 +0900

    tuto Init

---
+++ app/src/main/java/com/example/rssreader/MainActivity.kt
@@ -0,0 +1,115 @@
+package com.example.rssreader
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+//
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.ListItem
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+//
+import com.example.rssreader.ui.theme.RSSReaderTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val people = listOf(
+            "Jo",
+            "Ja",
+            "DJ",
+            "Lu",
+            "Mat",
+            "Jo",
+            "Ja",
+            "DJ",
+            "Lu",
+            "Mat",
+            "Jo",
+            "Ja",
+            "DJ",
+            "Lu",
+            "Mat",
+            "Jo",
+            "Ja",
+            "DJ",
+            "Lu",
+            "Mat"
+        )
+
+        setContent {
+            RSSReaderTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android", modifier = Modifier.padding(innerPadding)
+                    )
+                    LazyColumn {
+//                        items(people) { person ->
+//                            Text(text = person)
+//                        }
+                        items(people) {
+                            ListItem(it)
+//                            person -> ListItem(person) <- 이거 줄인게 "ListItem(it)" 임
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!", modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        RSSReaderTheme {
+            Greeting("Android")
+        }
+    }
+}
+
+@Composable
+fun ListItem(name: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+//        Column {
+        Row {
+            Image(
+                // R <- Resource(res), drawable <- 디렉토리명, baseline_person_24 <- 파일명
+                painter = painterResource(id = R.drawable.baseline_person_24),
+                contentDescription = "Photo of person",
+                modifier = Modifier
+                    .width(100.dp)
+                    .height(100.dp)
+            )
+            Text(
+                text = name, modifier = Modifier.padding(24.dp)
+            );
+        }
+    }
+}
