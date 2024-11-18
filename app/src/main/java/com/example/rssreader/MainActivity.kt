b9019083a77e22d04531158340b1a99f6940284a
liam <wonhyuk1994@gmail.com>
2024-11-18 18:41:54 +0900

    Code Base는 Android_APP_tuto로 함, Simple Webview APP reference

--- app/src/main/java/com/example/rssreader/MainActivity.kt
+++ app/src/main/java/com/example/rssreader/MainActivity.kt
@@ -17,17 +17,75 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-//
+// 커스텀 Import
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.ListItem
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+// WebView sdk
+import android.webkit.WebView
+import android.webkit.WebViewClient
+//
+import androidx.appcompat.app.AppCompatActivity
 //
 import com.example.rssreader.ui.theme.RSSReaderTheme
 import kotlin.reflect.typeOf
 
 class MainActivity : ComponentActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webview)
+
+        webView.settings.javaScriptEnabled = true // JavaScript 사용을 허용
+        webView.settings.domStorageEnabled = true // DOM storage 활성화 // 로컬 스토리지 사용( Token 때문에 필요함 )
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+        webView.settings.allowFileAccessFromFileURLs = true
+        webView.settings.allowUniversalAccessFromFileURLs = true
+
+        webView.webViewClient = WebViewClient() // 링크 클릭 시 새 창이 아닌 WebView 내에서 열리도록 설정
+
+        webView.loadUrl("https://app.cocabot.com/") // 원하는 URL로 변경
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // 액티비티가 화면에 나타날 준비가 되었을 때 수행할 작업
+        //TODO : 시작시 회사 로고 보이게
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // UI 업데이트나 리스너 등록
+        //TODO : 앱이 백그라운드 -> 포그라운드때 핸들링
+    }
+
+    //TODO : (SharedPreferences 사용)로그인정보 기억하다가 앱실행시, 자동 로그인
+
+//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        if (requestCode == PERMISSION_REQUEST_CODE) {
+//            // 권한 요청 결과 처리
+//            //TODO : 앱에서 권한이 필요 할 때 요청하는 핸들러
+//        }
+//    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack() // WebView에서 뒤로 가기
+        } else {
+            super.onBackPressed() // 기본 뒤로 가기
+        }
+    }
+}
+    /*
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -183,7 +241,7 @@ fun RSSItemVideo(items: RSSItem) {
         )
     }
 }
-
+*/
 
 
 
